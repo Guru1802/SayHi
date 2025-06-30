0c41ca7754820b85b73a958b4a3156251741e898
Guru <guruvedu@gmail.com>
2025-06-30 10:59:15 -0400

    Set everything to work on the frontend pages.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"; //provides us featues to build API quickly (eg. routes)
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,7 @@ app.use(express.json()) //allows to extract the json data from the body
 app.use(cookieParser()); //allows to parse the cookie
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
